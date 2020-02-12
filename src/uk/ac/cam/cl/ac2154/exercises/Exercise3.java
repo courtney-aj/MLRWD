eb8dcf987765ffc72ca3f4ad185c26e23f1ac3fb
anastasiacourtney <ac2154@cam.ac.uk>
2020-02-12 16:41:03 +0000

    Exercise 5 Skeleton Code

--- src/uk/ac/cam/cl/ac2154/exercises/Exercise3.java
+++ src/uk/ac/cam/cl/ac2154/exercises/Exercise3.java
@@ -1,7 +1,73 @@
 package uk.ac.cam.cl.ac2154.exercises;
+import javafx.util.Pair;
+import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.Tokenizer;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
 
 public class Exercise3 {
 
-    //public
+    public LinkedList<Pair<String, Double>> getFreqs (Set<Path> dataSet, Path lexiconFile) throws IOException {
+
+        SortedMap<String, Double> freqs = new TreeMap<String, Double>();
+        LinkedList<Pair<String, Double>> words = new LinkedList<>();
+        Double total = 0.0;
+        Double milestone = 1.0;
+        LinkedList<Integer> unique = new LinkedList<Integer>();
+        int uniqueTokens = 0;
+
+        for (Path p : dataSet) {
+            List<String> tokens = Tokenizer.tokenize(p);
+            for (String token : tokens) {
+                if (!freqs.containsKey(token)){
+                    uniqueTokens ++;
+                }
+                freqs.merge(token, 1.0, Double::sum);
+                if (total == milestone){
+                    unique.add(uniqueTokens);
+                    milestone *= 2.0;
+                }
+            }
+        }
+        for (String word : freqs.keySet()){
+            words.add(new Pair(word, freqs.get(word)));
+        }
+        Collections.sort(words, Comparator.comparing(p -> -p.getValue()));
+        return words;
+    }
+
+    public LinkedList<Pair<Integer,Integer>> HeapsLaw (Set<Path> dataSet, Path lexiconFile) throws IOException {
+
+        SortedMap<String, Double> freqs = new TreeMap<>();
+        int total = 0;
+        Double milestone = 1.0;
+        LinkedList<Pair<Integer, Integer>> unique = new LinkedList<>();
+        int uniqueTokens = 0;
+
+        for (Path p : dataSet) {
+            List<String> tokens = Tokenizer.tokenize(p);
+            for (String token : tokens) {
+                if (!freqs.containsKey(token)){
+                    uniqueTokens ++;
+                }
+                freqs.merge(token, 1.0, Double::sum);
+                total++;
+                if (milestone.intValue() == total){
+                    unique.add(new Pair<Integer, Integer>(uniqueTokens, total));
+                    milestone *= 2.0;
+                }
+            }
+        }
+        return unique;
+    }
+
+    public double exFreq(int rank, double grad, double yint){
+        double logrank = java.lang.Math.log(rank + 1);
+        double ans = java.lang.Math.exp(yint + logrank*grad);
+        return ans;
+
+    }
+
 
 }
--- src/uk/ac/cam/cl/mlrd/testing/Exercise3Tester.java
+++ src/uk/ac/cam/cl/mlrd/testing/Exercise3Tester.java
@@ -1,26 +1,111 @@
 package uk.ac.cam.cl.mlrd.testing;
 
+import javafx.util.Pair;
 import uk.ac.cam.cl.ac2154.exercises.Exercise3;
-import uk.ac.cam.cl.ac2154.exercises.Exercise4;
-import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.DataPreparation1;
-import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.IExercise4;
-import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.Sentiment;
-import uk.ac.cam.cl.mlrd.utils.DataSplit;
+import uk.ac.cam.cl.mlrd.utils.BestFit;
+import uk.ac.cam.cl.mlrd.utils.ChartPlotter;
 
+import java.awt.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.*;
+import java.lang.Math;
 
 public class Exercise3Tester {
-    static final Path dataDirectory = Paths.get("data/sentiment_dataset");
+
+    static final Path dataDirectory = Paths.get("data/large_dataset");
 
     public static void main(String[] args) throws Exception {
 
-        Path sentimentFile = dataDirectory.resolve("review_sentiment");
 
         Exercise3 implementation = new Exercise3();
 
         Path lexiconFile = Paths.get("data/sentiment_lexicon");
-}
+
+        Set<Path> dataSet = new HashSet<>();
+
+        DirectoryStream<Path> files = Files.newDirectoryStream(dataDirectory);
+        for (Path item : files) {
+            dataSet.add(item);
+        }
+        LinkedList<Pair<String, Double>> hold = implementation.getFreqs(dataSet, lexiconFile);
+        /*for(Pair<String, Double> p : hold) {
+            System.out.println(p);
+        }*/
+
+        LinkedList<String> mylex = new LinkedList<>();
+        mylex.add(0,"love");
+        mylex.add(0,"hate");
+        mylex.add(0,"disappoint");
+        mylex.add(0,"annoying");
+        mylex.add(0,"classic");
+        mylex.add(0,"slow");
+        mylex.add(0,"fun");
+        mylex.add(0,"beautiful");
+        mylex.add(0,"great");
+        mylex.add(0,"awful");
+
+
+        LinkedList<BestFit.Point> points2 = new LinkedList<>();
+
+        LinkedList<BestFit.Point> points = new LinkedList<>();
+        /*for(Double i=0.0; i<10000.0; i++){
+            points.add(new BestFit.Point(i+1.0, hold.get(i.intValue()).getValue()));
+            if(mylex.contains(hold.get(i.intValue()).getKey())) {
+                points2.add(new BestFit.Point(i + 1.0, hold.get(i.intValue()).getValue()));
+            }
+        }*/
+        //ChartPlotter.plotLines(points, points2);
+        System.out.println("Done");
+
+
+        LinkedList<BestFit.Point> points3 = new LinkedList<>();
+        /*for(double i = 0.0; i<10000.0; i++){
+            points3.add(new BestFit.Point(java.lang.Math.log(i+1.0), java.lang.Math.log(hold.get((int) i).getValue())));
+        }*/
+        int i=0;
+
+        Map<BestFit.Point, Double> series = new HashMap<>();
+        /*for (BestFit.Point p : points3){
+            series.put(p, hold.get(i).getValue());
+            i++;
+        }*/
+        BestFit.Line line = BestFit.leastSquares(series);
+        double grad = line.gradient;
+        double yint = line.yIntercept;
+
+
+        LinkedList<BestFit.Point> points4 = new LinkedList<>();
+        points4.add(new BestFit.Point(0, yint));
+        points4.add(new BestFit.Point(-yint/grad, 0));
+
+
+        //ChartPlotter.plotLines(points3, points4);
+        System.out.println(implementation.exFreq(0, grad, yint));
+
+        double k = implementation.exFreq(0, grad, yint);
+        double alpha = -grad;
+
+        System.out.println("k = " + k);
+        System.out.println("alpha = " + alpha);
+
+        LinkedList<Pair<Integer, Integer>> uniqtok = implementation.HeapsLaw(dataSet, lexiconFile);
+        LinkedList<BestFit.Point> total = new LinkedList<>();
+        LinkedList<BestFit.Point> heapslaw = new LinkedList<>();
+
+        double i2 = 0;
+        for(Pair<Integer, Integer> p: uniqtok){
+            heapslaw.add(new BestFit.Point(java.lang.Math.log(java.lang.Math.pow(2.0, i2)), java.lang.Math.log(p.getKey())));
+            total.add(new BestFit.Point(java.lang.Math.log(java.lang.Math.pow(2.0, i2)),java.lang.Math.log(p.getValue())));
+            i2++;
+        }
+
+        ChartPlotter.plotLines(heapslaw, total);
+    System.out.println("DONE");
+
+    }
+
 
 }
